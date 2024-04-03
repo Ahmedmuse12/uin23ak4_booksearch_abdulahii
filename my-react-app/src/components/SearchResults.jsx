c236fe65f30ac25610e0bfc7c257a73a4f525832
Ahmedmuse12 <111729465+Ahmedmuse12@users.noreply.github.com>
2024-04-03 08:33:47 +0200

    Update SearchResults.jsx

---
+++ my-react-app/src/components/SearchResults.jsx
@@ -0,0 +1,21 @@
+import Bookcard from "./Bookcard";
+
+export default function SearchResults({content}){
+    const allBooks = content?.docs;
+    console.log(content?.docs)
+
+    return (
+    <span className="display-Books">
+        {allBooks?.map(singleBook => 
+        <Bookcard className="card" 
+            key={singleBook._version_} 
+            title={singleBook.title} 
+            first_publish_year={singleBook.first_publish_year}
+            author_name={singleBook.author_name}
+            ratings_average={singleBook.ratings_average}
+            coverImage={singleBook.cover_i}
+            >
+        </Bookcard>)}
+    </span>
+    )
+}
