0fd360681f2f185b6de85e6232f59cc0648ee29f
Ahmedmuse12 <111729465+Ahmedmuse12@users.noreply.github.com>
2024-04-03 08:28:41 +0200

    Update layout

---
+++ my-react-app/src/components/Layout.jsx
@@ -0,0 +1,16 @@
+import SearchBar from "./SearchBar";
+
+export default function Layout({children, query, setQuery}){
+    return(
+        <>
+        <header>
+            <nav>
+                <h1>BookSearch</h1>
+            </nav>
+        </header>
+        <main>
+           
+        </main>
+        </>
+    )
+}
