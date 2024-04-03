d2690cd702c1860727da60344c734aa44ef9bd26
Ahmedmuse12 <111729465+Ahmedmuse12@users.noreply.github.com>
2024-04-03 08:25:40 +0200

    Bookcard med kommentar

--- my-react-app/src/components/Bookcard.jsx
+++ my-react-app/src/components/Bookcard.jsx
@@ -8,6 +8,8 @@ export default function Bookcard({keyBook, title, first_publish_year, author_nam
         <p>Author: {author_name}</p>
         <p>Average rating: {ratings_average}</p>
         <img src={`https://covers.openlibrary.org/b/id/${coverImage}-L.jpg`} alt={title}/>
+        {/* For å hente bilder for hver enkelt bok, benyttet jeg meg av veiledningen tilgjengelig i API-dokumentasjonen på openlibrary.org.*/}
+        {/* https://openlibrary.org/dev/docs/api/covers */}
         </article>
     </>
     )
