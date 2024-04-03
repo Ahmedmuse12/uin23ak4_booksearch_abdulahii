b0c200686ef5cd259672a44deefdce1377c0def1
Ahmedmuse12 <111729465+Ahmedmuse12@users.noreply.github.com>
2024-04-03 08:33:29 +0200

    updaterte searchbar med riktig innhold

--- my-react-app/src/components/SearchBar.jsx
+++ my-react-app/src/components/SearchBar.jsx
@@ -1,3 +1,5 @@
+
+import { useState } from "react"
 export default function SearchBar({query, setQuery}){
     const [search, setSearch] = useState("")
     
@@ -13,4 +15,18 @@ export default function SearchBar({query, setQuery}){
     const handleClick = (title)=>{
         setCurrentId(title)
         localStorage.setItem("karakterID", title)
-    }}
+    }
+
+return(
+    <>
+        <h2>Search for books</h2>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="search"></label>
+            <input type="text" id="search" placeholder="James Bond..." onChange={handleChange}></input>
+            <input type="submit" value="Søk"></input>
+        </form>
+        <ul>
+            {query?.map(book => <li key={book._version_}><Link to={book.title} onClick={()=>handleClick(book._version_)}>{book.title}</Link></li>)}
+        </ul>
+    </>)
+}
