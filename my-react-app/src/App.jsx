b42b0d8df0b33b16aa11cb9880d2ad2aa7ad9e15
Ahmedmuse12 <111729465+Ahmedmuse12@users.noreply.github.com>
2024-04-03 08:36:44 +0200

    Update App.jsx

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,44 @@
+
+import {Route, Routes} from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import Layout from './components/Layout'
+import './App.css'
+import SearchResults from './components/SearchResults'
+
+
+function App() {
+
+  const [query, setQuery] = useState("James+Bond")
+  const [content, setContent] = useState([])
+  
+
+
+   const getData = async()=>{
+    try {
+      const response = await fetch(`https://openlibrary.org/search.json?title=${query}`);
+      const data = await response.json();
+      setContent(data);
+
+    } catch {
+      console.error("En feil har oppstått.")
+    } 
+   }
+
+   useEffect(()=>{
+    getData()
+  
+  },[query])
+
+  
+
+  return (
+    <>
+      <Layout content={content} setQuery={setQuery}>
+        <Routes>
+          <Route index element={<SearchResults content={content}/>}/>
+        </Routes>
+      </Layout>
+    </>
+  )
+}
+export default App
